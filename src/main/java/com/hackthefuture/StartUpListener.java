5d7f0d3b3405220d4d6fde24e52d021e8a077fd4
Kaisum <kaisumaro445@hotmail.com>
2017-11-30 11:12:39 +0100

    reverted changes

--- src/main/java/com/hackthefuture/StartUpListener.java
+++ src/main/java/com/hackthefuture/StartUpListener.java
@@ -1,6 +1,9 @@
 package com.hackthefuture;
 
+import com.hackthefuture.DAL.models.Alert;
+import com.hackthefuture.DAL.models.Location;
 import com.hackthefuture.DAL.models.User;
+import com.hackthefuture.DAL.repositories.AlertRepository;
 import com.hackthefuture.DAL.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -19,6 +22,8 @@ public class StartUpListener implements ApplicationListener<ContextRefreshedEven
     @Autowired
     private UserRepository userRepository;
     @Autowired
+    private AlertRepository alertRepository;
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
@@ -34,8 +39,13 @@ public class StartUpListener implements ApplicationListener<ContextRefreshedEven
             user.setActive(true);
             userRepository.save(user);
             System.out.println("\n\n\nOk ");
-        }else{
-            System.out.println("\n\n\nOk ******************************************************");
+
+            Alert alert = new Alert();
+            alert.setUser(user);
+            alert.setLocation(new Location(51.260197,4.402771));
+            alert.setDetails("test location");
+
+            alertRepository.insert(alert);
         }
 
     }
