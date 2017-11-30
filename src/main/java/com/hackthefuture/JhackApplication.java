19ee36ba245c8a3211fe3e4c1cf777af05cc7033
ZhaodiFeng <fengzhaodi1993@gmail.com>
2017-11-30 09:26:03 +0100

    Project Setup

---
+++ src/main/java/com/hackthefuture/JhackApplication.java
@@ -0,0 +1,12 @@
+package com.hackthefuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhackApplication.class, args);
+	}
+}
